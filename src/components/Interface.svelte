18a0f6fb18a839420cb4f0341c999ee2415ee626
b1mind <brent@1mindsolutions.com>
2021-04-02 15:43:08 -0600

    💢 failed abstraction

--- src/components/Interface.svelte
+++ src/components/Interface.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte'
   import { gsap } from 'gsap'
   import { Draggable } from 'gsap/Draggable'
+  import Toppings from './Toppings.svelte'
 
   gsap.registerPlugin(Draggable)
 
@@ -40,6 +41,7 @@
     gsap.to(toppings.children, { duration: 1, scale: 1 })
 
     Draggable.create(toppings.children, {
+      //fixme: svg bounding rec not working ?
       bounds: document.querySelector('[data-pizzaBox]'),
 
       onDrag: function (e) {
@@ -49,15 +51,23 @@
       },
 
       onDragEnd: function (e) {
-        checkHit(this)
-          ? gsap.to(this.target, {
-              duration: 0.25,
-              scale: 0,
-              transformOrigin: 'center center',
-            })
-          : gsap.to(this.target, { duration: 0.25, x: 0, y: 0 })
+        if (checkHit(this)) {
+          gsap.to(this.target, {
+            duration: 0.25,
+            scale: 0,
+            transformOrigin: 'center center',
+          })
+
+          // make a function that checks what topping based on dataset value?
+          // clone the topping svg or just topping?
+          console.log(this.target)
+        } else {
+          gsap.to(this.target, { duration: 0.25, x: 0, y: 0 })
+        }
       },
     })
+
+    //todo: clean up event listeners?
   }
 </script>
 
@@ -75,52 +85,42 @@
 
     <g bind:this={pizza}>
       <circle data-pizza cx="180" cy="251" r="75" fill="#C4C4C4" />
+      <!-- clones of toppings go here  -->
     </g>
-
-    <!--   <g data-toppings fill="#D3FF77" bind:this={toppings}>
-    <path data-topping="pepper" d="M30 483h50v50H30z" />
-    <path d="M30 569h50v50H30z" />
-    <path d="M116 483h50v50h-50z" />
-    <path d="M116 569h50v50h-50z" />
-    <path d="M201 483h50v50h-50z" />
-    <path d="M201 569h50v50h-50z" />
-    <path d="M286 483h50v50h-50z" />
-    <path d="M286 569h50v50h-50z" />
-  </g> -->
 
     <g class="sizes" on:click={resizePizza}>
       <circle data-size="12" cx="66" cy="552" r="30" />
       <circle data-size="15" cx="158.5" cy="551.5" r="40" />
       <circle data-size="20" cx="276" cy="547" r="50" />
     </g>
-
-    <!-- <g class="sizes-text">
-    <path
-      d="M61.27 558h-1.09v-7.23l-2.18.8v-.98l3.1-1.16h.17V558zm8.77 0h-5.6v-.78l2.96-3.28c.44-.5.74-.9.9-1.2.17-.32.25-.64.25-.97 0-.45-.13-.81-.4-1.1a1.41 1.41 0 00-1.08-.43c-.54 0-.96.16-1.26.47-.3.3-.44.73-.44 1.27h-1.09c0-.78.26-1.42.76-1.9.5-.49 1.18-.73 2.03-.73.8 0 1.42.21 1.88.63.46.41.7.97.7 1.66 0 .84-.54 1.84-1.61 3l-2.29 2.48h4.29v.88zm1.4-6.13l-.61-.42c.36-.5.55-1.04.56-1.6V549h1.07v.76c0 .4-.1.78-.29 1.17-.19.39-.43.7-.73.94zm1.8 0l-.61-.42c.36-.5.55-1.04.56-1.6V549h1.07v.76c0 .4-.1.78-.3 1.17-.18.39-.42.7-.72.94z" />
-    <path
-      d="M153.27 556h-1.09v-7.23l-2.18.8v-.98l3.1-1.16h.17V556zm3.67-4.28l.44-4.25h4.37v1h-3.45l-.26 2.33c.42-.25.9-.37 1.43-.37.77 0 1.39.26 1.85.77a3 3 0 01.68 2.08c0 .88-.24 1.57-.71 2.08a2.6 2.6 0 01-1.99.76 2.7 2.7 0 01-1.84-.63 2.5 2.5 0 01-.82-1.73h1.03c.06.48.24.85.52 1.1.28.25.65.37 1.11.37.5 0 .9-.17 1.18-.51.3-.35.44-.82.44-1.43 0-.57-.16-1.02-.47-1.37-.3-.35-.72-.52-1.24-.52-.47 0-.84.1-1.11.31l-.29.24-.87-.23zm6.5-1.85l-.61-.42c.36-.5.55-1.04.56-1.6V547h1.07v.76c0 .4-.1.78-.29 1.17-.19.39-.43.7-.73.94zm1.8 0l-.61-.42c.36-.5.55-1.04.56-1.6V547h1.07v.76c0 .4-.1.78-.3 1.17-.18.39-.42.7-.72.94z" />
-    <path
-      d="M273.3 554h-5.6v-.78l2.96-3.28c.44-.5.74-.9.9-1.2.17-.32.26-.64.26-.97 0-.45-.14-.81-.4-1.1a1.41 1.41 0 00-1.09-.43c-.53 0-.95.16-1.26.47-.3.3-.44.73-.44 1.27h-1.08c0-.78.25-1.42.75-1.9.5-.49 1.19-.73 2.03-.73.8 0 1.42.21 1.88.63.47.41.7.97.7 1.66 0 .84-.54 1.84-1.6 3l-2.3 2.48h4.29v.88zm6.5-3.64c0 1.27-.22 2.21-.65 2.83-.44.62-1.11.93-2.04.93-.9 0-1.58-.3-2.02-.9-.44-.61-.66-1.51-.68-2.71v-1.45c0-1.25.22-2.18.65-2.8.44-.6 1.12-.9 2.04-.9.92 0 1.6.29 2.03.88.43.58.66 1.49.67 2.72v1.4zm-1.09-1.48c0-.92-.13-1.59-.38-2-.26-.43-.67-.64-1.23-.64-.55 0-.96.21-1.21.63a3.88 3.88 0 00-.4 1.93v1.73c0 .92.14 1.6.4 2.05.27.43.68.65 1.22.65.54 0 .94-.2 1.2-.62.25-.41.4-1.07.4-1.96v-1.77zm2.73-1l-.61-.43c.36-.5.55-1.04.56-1.6V545h1.07v.76c0 .4-.1.78-.29 1.17-.19.39-.43.7-.73.94zm1.8 0l-.61-.43c.36-.5.55-1.04.56-1.6V545h1.07v.76c0 .4-.1.78-.3 1.17-.18.39-.42.7-.72.94z" />
-  </g> -->
   </svg>
 
   <div data-toppings bind:this={toppings}>
-    <svg data-orderBox fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="50" height="50">
+      <rect width="50" height="50" fill="#D3FF77" />
+    </svg>
+    <svg width="50" height="50">
+      <rect width="50" height="50" fill="#D3FF77" />
+    </svg>
+    <svg width="50" height="50">
+      <rect width="50" height="50" fill="#D3FF77" />
+    </svg>
+    <svg width="50" height="50">
       <rect width="50" height="50" fill="#D3FF77" />
     </svg>
   </div>
-  <!-- <svg data-orderBox viewBox="0 0 306 136" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g data-toppings fill="#D3FF77">
-      <rect width="50" height="50" fill="#D3FF77" />
-      <rect y="86" width="50" height="50" fill="#D3FF77" />
-      <rect x="86" width="50" height="50" fill="#D3FF77" />
-      <rect x="86" y="86" width="50" height="50" fill="#D3FF77" />
-      <rect x="171" width="50" height="50" fill="#D3FF77" />
-      <rect x="171" y="86" width="50" height="50" fill="#D3FF77" />
-      <rect x="256" width="50" height="50" fill="#D3FF77" />
-      <rect x="256" y="86" width="50" height="50" fill="#D3FF77" />
-    </g>
-  </svg> -->
+
+  <!-- <div data-toppings bind:this={toppings}>
+    <Toppings topping="ham" />
+    <Toppings topping="sausage" viewBox="100 0 50 50" />
+    <Toppings viewBox="200 0 50 50" />
+    <Toppings viewBox="300 0 50 50" />
+    <Toppings viewBox="400 0 50 50" />
+    <Toppings viewBox="0 100 50 50" />
+    <Toppings viewBox="0 200 50 50" />
+    <Toppings viewBox="0 300 50 50" />
+    <Toppings viewBox="0 400 50 50" />
+  </div> -->
 </div>
 
 <style type="text/scss">
@@ -135,11 +135,6 @@
     // max-height: 100vh;
     grid-column: 2;
     grid-row: 1 / span 2;
-  }
-
-  [data-orderBox] {
-    width: 50px;
-    height: 50px;
   }
 
   [data-toppings] {
---
+++ src/components/Toppings.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let viewBox = '0 0 50 50'
+  export let topping = 'ham'
+</script>
+
+<svg
+  data-topping={topping}
+  class="oderBox"
+  data-orderBox
+  {viewBox}
+  width="50"
+  height="50"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg">
+  <rect width="50" height="50" fill="pink" />
+  <rect x="100" width="50" height="50" fill="brown" />
+  <rect x="200" width="50" height="50" fill="green" />
+  <rect x="300" width="50" height="50" fill="plum" />
+  <rect x="400" width="50" height="50" fill="skyblue" />
+  <rect y="100" width="50" height="50" fill="blue" />
+  <rect y="200" width="50" height="50" fill="teal" />
+  <rect y="300" width="50" height="50" fill="red" />
+  <rect y="400" width="50" height="50" fill="purple" />
+</svg>
+
+<style type="text/scss">
+  .oderBox {
+    width: 50px;
+    height: 50px;
+  }
+</style>
